d89320ff1e749305b135c45492e86f88775a1e79
steveyxz <stevenzhu2009@outlook.com>
2022-05-22 15:13:11 +1000

    Added the recipe option to lucky blocks

--- Plugin/src/main/java/me/partlysunny/blocks/LuckyBlockType.java
+++ Plugin/src/main/java/me/partlysunny/blocks/LuckyBlockType.java
@@ -1,29 +1,37 @@
 package me.partlysunny.blocks;
 
+import de.tr7zw.nbtapi.NBTItem;
 import me.partlysunny.ConsoleLogger;
 import me.partlysunny.SimpleLuckyBlocksCore;
 import me.partlysunny.particle.BlockParticleEffect;
 import me.partlysunny.particle.EffectType;
 import me.partlysunny.util.Util;
+import me.partlysunny.util.classes.ItemBuilder;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.Particle;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.annotation.Nullable;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public record LuckyBlockType(String id, String displayName, Material blockType, @Nullable ItemStack innerItem, String lootTable,
-                             BlockParticleEffect e) {
+                             BlockParticleEffect e, @Nullable ShapedRecipe r) {
 
     private static final Map<String, LuckyBlockType> types = new HashMap<>();
 
     public static void registerType(LuckyBlockType type) {
         types.put(type.id, type);
+        JavaPlugin.getPlugin(SimpleLuckyBlocksCore.class).getServer().addRecipe(type.r);
     }
 
     public static void unregisterType(String id) {
@@ -52,6 +60,7 @@ public record LuckyBlockType(String id, String displayName, Material blockType, 
         String displayName = "Lucky Block";
         ItemStack innerItem = null;
         BlockParticleEffect e = null;
+        ShapedRecipe sr = null;
         if (name.contains("displayName")) {
             displayName = name.getString("displayName");
         }
@@ -70,7 +79,35 @@ public record LuckyBlockType(String id, String displayName, Material blockType, 
             Particle particle = Particle.valueOf(effectInfo.getString("particle"));
             e = new BlockParticleEffect(particle, frequency, type);
         }
-        registerType(new LuckyBlockType(childName.substring(0, childName.length() - 4), displayName, mat, innerItem, lootTable, e));
+        String substring = childName.substring(0, childName.length() - 4);
+        if (name.contains("recipe")) {
+            ConfigurationSection recipeInfo = name.getConfigurationSection("recipe");
+            List<String> slots = recipeInfo.getStringList("slots");
+            Map<Character, Material> keys = new HashMap<>();
+            ConfigurationSection keysSection = recipeInfo.getConfigurationSection("keys");
+            for (String c : keysSection.getKeys(true)) {
+                keys.put(c.charAt(0), Material.getMaterial(keysSection.getString(c)));
+            }
+            ItemStack block;
+            if (innerItem == null) {
+                block = ItemBuilder.builder(mat).setName(displayName).build();
+            } else {
+                block = innerItem.clone();
+                ItemMeta itemMeta = block.getItemMeta();
+                itemMeta.setDisplayName(displayName);
+                block.setItemMeta(itemMeta);
+            }
+            NBTItem nbti = new NBTItem(block);
+            nbti.setString("luckyType", substring);
+            nbti.applyNBT(block);
+            ShapedRecipe r = new ShapedRecipe(new NamespacedKey(JavaPlugin.getPlugin(SimpleLuckyBlocksCore.class), substring), block);
+            r.shape(slots.toArray(new String[0]));
+            for (Character c : keys.keySet()) {
+                r.setIngredient(c, keys.get(c));
+            }
+            sr = r;
+        }
+        registerType(new LuckyBlockType(substring, displayName, mat, innerItem, lootTable, e, sr));
     }
 
 }
